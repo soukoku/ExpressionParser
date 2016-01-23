59e977063b3a1af8e122b82921893aad892b3b10
Eugene Wang <yinchun.wang@gmail.com>
2016-01-23 17:31:49 -0500

    A highly naive low level tokenizer with tests.

---
+++ src/ExpressionParser/RawTokenizer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Soukoku.ExpressionParser
+{
+    /// <summary>
+    /// The initial input string tokenizer.
+    /// </summary>
+    public class RawTokenizer
+    {
+        static readonly char[] __defaultSymbols = new[]
+            {
+                '+', '-', '*', '/', '=', '%', '^',
+                ',', '<', '>', '&', '|', '!',
+                '(', ')', '{', '}', '[', ']'
+            };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RawTokenizer" /> class.
+        /// </summary>
+        /// <param name="symbols">The char values to count as symbols. If null a default set will be used.</param>
+        public RawTokenizer(char[] symbols = null)
+        {
+            Symbols = symbols ?? __defaultSymbols;
+        }
+
+        /// <summary>
+        /// Gets the char values that count as symbols.
+        /// </summary>
+        /// <value>
+        /// The symbols.
+        /// </value>
+        public char[] Symbols { get; private set; }
+
+        /// <summary>
+        /// Splits the specified input into a list of raw token values using whitespace and symbols.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns></returns>
+        public IList<RawToken> Tokenize(string input)
+        {
+            var tokens = new List<RawToken>();
+
+            if (input != null)
+            {
+                RawToken lastToken = null;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var ch = input[i];
+                    if (char.IsWhiteSpace(ch))
+                    {
+                        lastToken = NewTokenIfNecessary(tokens, lastToken, RawTokenType.Whitespace);
+                    }
+                    else if (Symbols.Contains(ch))
+                    {
+                        lastToken = NewTokenIfNecessary(tokens, lastToken, RawTokenType.Symbol);
+                    }
+                    else
+                    {
+                        lastToken = NewTokenIfNecessary(tokens, lastToken, RawTokenType.Literal);
+                    }
+                    lastToken.ValueBuilder.Append(ch);
+                }
+            }
+            return tokens;
+        }
+
+        private RawToken NewTokenIfNecessary(List<RawToken> tokens, RawToken lastToken, RawTokenType curTokenType)
+        {
+            if (lastToken == null || lastToken.Type != curTokenType)
+            {
+                lastToken = new RawToken(curTokenType);
+                tokens.Add(lastToken);
+            }
+            return lastToken;
+        }
+    }
+
+    /// <summary>
+    /// A token split from the initial text input.
+    /// </summary>
+    public class RawToken
+    {
+        internal RawToken(RawTokenType type)
+        {
+            Type = type;
+            ValueBuilder = new StringBuilder();
+        }
+
+        /// <summary>
+        /// Gets the token type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public RawTokenType Type { get; private set; }
+        internal StringBuilder ValueBuilder { get; private set; }
+
+        /// <summary>
+        /// Gets the token value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public string Value { get { return ValueBuilder.ToString(); } }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+
+    /// <summary>
+    /// Indicates the raw token type.
+    /// </summary>
+    public enum RawTokenType
+    {
+        /// <summary>
+        /// Invalid token type.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Token is whitespace.
+        /// </summary>
+        Whitespace,
+        /// <summary>
+        /// Token is a symbol.
+        /// </summary>
+        Symbol,
+        /// <summary>
+        /// Token is not symbol or whitespace.
+        /// </summary>
+        Literal,
+    }
+}
---
+++ tests/ExpressionParserTests/RawTokenizer_Should.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Soukoku.ExpressionParser
+{
+    [TestClass]
+    public class RawTokenizer_Should
+    {
+        #region utility
+
+        private void ExpectTokens(params string[] expectedTokens)
+        {
+            var actualTokens = new RawTokenizer().Tokenize(_input);
+            CollectionAssert.AreEqual(expectedTokens, actualTokens.Select(tk => tk.Value).ToList());
+        }
+
+        string _input;
+        private void GivenInput(string input)
+        {
+            _input = input;
+        }
+
+        #endregion
+
+
+        [TestMethod]
+        public void Return_Empty_List_When_Null()
+        {
+            GivenInput(null);
+            ExpectTokens();
+        }
+        [TestMethod]
+        public void Return_Empty_List_When_Empty()
+        {
+            GivenInput("");
+            ExpectTokens();
+        }
+
+        [TestMethod]
+        public void Return_Single_Char_Value()
+        {
+            GivenInput("a");
+            ExpectTokens("a");
+        }
+
+        [TestMethod]
+        public void Return_Single_MultiChar_Value()
+        {
+            GivenInput("ab");
+            ExpectTokens("ab");
+        }
+
+        [TestMethod]
+        public void Return_Multiple_Values_Split_By_Whitespace()
+        {
+            GivenInput("ab cd");
+            ExpectTokens("ab", " ", "cd");
+        }
+
+        [TestMethod]
+        public void Return_Multiple_Values_Split_By_Symbols()
+        {
+            GivenInput("ab+cd");
+            ExpectTokens("ab", "+", "cd");
+        }
+
+        [TestMethod]
+        public void Return_Multiple_Values_Split_By_Symbols_And_Whitespace()
+        {
+            GivenInput(" ab  +  cd ");
+            ExpectTokens(" ", "ab", "  ", "+", "  ", "cd", " ");
+        }
+    }
+}
