1ca6a9b797126c0e9e508155078f0d2fb21a0418
Eugene Wang <yinchun.wang@gmail.com>
2016-01-23 16:21:48 -0500

    Setup code structures.

---
+++ src/ExpressionParser/Properties/VersionInfo.cs
@@ -0,0 +1,20 @@
+﻿using Soukoku.ExpressionParser;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Yin-Chun Wang")]
+[assembly: AssemblyCopyright("Copyright © Yin-Chun Wang 2016")]
+
+[assembly: AssemblyVersion(VersionInfo.MajorVersion)]
+[assembly: AssemblyFileVersion(VersionInfo.BuildVersion)]
+[assembly: AssemblyInformationalVersion(VersionInfo.BuildVersion)]
+
+namespace Soukoku.ExpressionParser
+{
+    static class VersionInfo
+    {
+        // keep this same in major releases
+        public const string MajorVersion = "1.0.0.0";
+        // change this for each nuget release
+        public const string BuildVersion = "1.0.0";
+    }
+}
